df3fa7fad3c24ea59c2d8d779c5b0b56e557a15d
James Petry <gavilancomun@gmail.com>
2015-05-21 10:47:08 +0100

    cells

--- src/kakuro/core.clj
+++ src/kakuro/core.clj
@@ -19,3 +19,11 @@
   Cell
   (draw [this] (format "   %2d\\%2d  " (:down this) (:across this))))
 
+(defrecord ValueCell [values]
+  Cell
+  (draw [this]
+    (let [values (:values this)]
+      (if (= 1 (count values))
+        (first (map #(str "     " % "    ") values))
+        (str " " (apply str (map #(if (contains? values %) % ".") (range 1 10))))))))
+
