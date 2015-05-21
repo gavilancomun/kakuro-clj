4a25ae21d16fb00d841503a52be89a91e9f091d6
James Petry <gavilancomun@gmail.com>
2015-05-21 11:25:29 +0100

    draw

--- src/kakuro/core.clj
+++ src/kakuro/core.clj
@@ -3,23 +3,23 @@
 (defprotocol Cell
   (draw [this]))
 
-(defrecord EmptyCell []
+(defrecord Empty[]
   Cell
   (draw [this] "   -----  "))
 
-(defrecord DownCell [down]
+(defrecord Down[down]
   Cell
   (draw [this] (format "   %2d\\--  " (:down this))))
 
-(defrecord AcrossCell [across]
+(defrecord Across[across]
   Cell
   (draw [this] (format "   --\\%2d  " (:across this))))
 
-(defrecord DownAcrossCell [down across]
+(defrecord DownAcross[down across]
   Cell
   (draw [this] (format "   %2d\\%2d  " (:down this) (:across this))))
 
-(defrecord ValueCell [values]
+(defrecord Value[values]
   Cell
   (draw [this]
     (let [values (:values this)]
@@ -29,3 +29,27 @@
                       (map #(if (contains? values %) % "."))
                       (apply str)))))))
 
+(def val9 (->Value #{1 2 3 4 5 6 7 8 9}))
+(def e (->Empty))
+(defn d [n] (->Down n))
+(defn a [n] (->Across n))
+(defn da [d a] (->DownAcross d a))
+
+(defn create-values [n]
+  (->> (range 1 (+ 1 n))
+       (map (fn [_] val9))
+       (into [])))
+
+(def grid1 [
+            [e (d 4) (d 22) e (d 16) (d 3)]
+            [(a 3) val9 val9 (da 16 6) val9 val9 ]
+            [(a 18) val9 val9 val9 val9 val9]
+            [e (da 17 23) val9 val9 val9 (d 14)]
+            [(a 9) val9 val9 (a 6) val9 val9]
+            [(a 15) val9 val9 (a 2) val9 val9]
+           ])
+
+(defn draw-row [row]
+  (str (apply str (map draw row)) "\n"))
+
+(defn draw-grid [grid] (apply str (map draw-row grid)))
