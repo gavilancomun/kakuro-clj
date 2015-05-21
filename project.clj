8b135abd468f5208d176e3cc7d534e1fbefb9340
James Petry <gavilancomun@gmail.com>
2015-05-21 10:36:51 +0100

    cells

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject kakuro-clj "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 ])
--- src/kakuro/core.clj
+++ src/kakuro/core.clj
@@ -1,4 +1,21 @@
-(namespace kakuro.core)
+(ns kakuro.core)
 
+(defprotocol Cell
+  (draw [this]))
 
+(defrecord EmptyCell []
+  Cell
+  (draw [this] "   -----  "))
+
+(defrecord DownCell [down]
+  Cell
+  (draw [this] (format "   %2d\\--  " (:down this))))
+
+(defrecord AcrossCell [across]
+  Cell
+  (draw [this] (format "   --\\%2d  " (:across this))))
+
+(defrecord DownAcrossCell [down across]
+  Cell
+  (draw [this] (format "   %2d\\%2d  " (:down this) (:across this))))
 
