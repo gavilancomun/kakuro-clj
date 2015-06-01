be3171393e0b34968999ed09f2ea93b5695a5470
James Petry <gavilancomun@gmail.com>
2015-06-01 12:29:42 +0100

    test

--- test/kakuro/test.clj
+++ test/kakuro/test.clj
@@ -6,15 +6,9 @@
             [clojure.test.check.clojure-test :as ct]
             [kakuro.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
-
-(ct/defspec first-element-is-min-after-sorting 
-         100 ;; the number of iterations for test.check to test
-         (prop/for-all [v (gen/not-empty (gen/vector gen/int))]
-           (= (apply min v)
-              (first (sort v)))))
+;;(deftest a-test
+;;  (testing "FIXME, I fail."
+;;    (is (= 0 1))))
 
 (ct/defspec test-transpose
   100
