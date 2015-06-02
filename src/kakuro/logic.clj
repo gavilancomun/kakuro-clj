7918f3af685eaf226dbf93586d5b05c0fbdb0217
James Petry <gavilancomun@gmail.com>
2015-06-02 11:23:28 +0100

    logic

--- src/kakuro/logic.clj
+++ src/kakuro/logic.clj
@@ -9,14 +9,18 @@
     (cl/fresh [a b]
       (cl/everyg #(fd/in % (fd/domain 1 2 3 4 5 6 7 8 9)) [a b])
       (fd/+ a b sum)
+      (fd/distinct [a b])
       (cl/== q [a b]))))
 
 (defn nary-plus [vars sum]
   )
 
 (defn solve-sum-n [n sum]
-  (let [vars (repeatedly n cl/lvar)]
+  (let [vars (repeatedly n cl/lvar)
+        accs (repeatedly (dec n) cl/lvar)]
     (cl/run* [q]
+      (cl/everyg #(fd/in % (apply fd/domain (range 1 (inc sum)))) accs)
       (cl/everyg #(fd/in % (apply fd/domain (range 1 4))) vars)
+      (fd/distinct vars)      
       (cl/== q vars))))
 
