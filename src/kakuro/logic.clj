28290ddd6dac13f46b4cc3e77613ef3023454917
James Petry <gavilancomun@gmail.com>
2015-06-02 11:51:36 +0100

    logic

--- src/kakuro/logic.clj
+++ src/kakuro/logic.clj
@@ -7,23 +7,27 @@
 
 (defn solve-sum [sum]
   (cl/run* [q]
-    (cl/fresh [a b]
-      (cl/everyg #(fd/in % (fd/domain 1 2 3 4 5 6 7 8 9)) [a b])
-      (fd/+ a b sum)
-      (fd/distinct [a b])
-      (cl/== q [a b]))))
+           (cl/fresh [a b]
+                     (cl/everyg #(fd/in % (fd/domain 1 2 3 4 5 6 7 8 9)) [a b])
+                     (fd/+ a b sum)
+                     (fd/distinct [a b])
+                     (cl/== q [a b]))))
 
 (defn nary-plus [sum vars]
   (cond
     (core/= 1 (count vars)) (cl/== (first vars) sum)
     (core/= 2 (count vars)) (fd/+ (first vars) (second vars) sum)
-    :else cl/succeed))
+    :else
+    (cl/fresh [next-sum]
+              (fd/- sum (last vars) next-sum)
+              (nary-plus next-sum (butlast vars)))))
 
 (defn solve-sum-n [n sum]
-  (let [vars (repeatedly n cl/lvar)]
-    (cl/run* [q]
-      (cl/everyg #(fd/in % (apply fd/domain (range 1 10))) vars)
-      (nary-plus sum vars)
-      (fd/distinct vars)      
-      (cl/== q vars))))
+  (if (core/> n 0)
+    (let [vars (repeatedly n cl/lvar)]
+      (cl/run* [q]
+               (cl/everyg #(fd/in % (apply fd/domain (range 1 10))) vars)
+               (nary-plus sum vars)
+               (fd/distinct vars)      
+               (cl/== q vars)))))
 
