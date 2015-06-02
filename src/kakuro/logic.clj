89c567200b7d6ac7c370fe129bb4682b6e6980f3
James Petry <gavilancomun@gmail.com>
2015-06-02 16:20:43 +0100

    logic

--- src/kakuro/logic.clj
+++ src/kakuro/logic.clj
@@ -45,18 +45,22 @@
              (cl/== q vars))))
 
 (defn lvar-row [row]
-  (mapv #(if (:values %) [% cl/lvar] [% nil]) row))
+  (mapv #(vector % (if (:values %) (cl/lvar) nil)) row))
 
 (defn lvar-grid [grid]
   (mapv lvar-row grid))
 
 (defn logic-pair [k [nvs vs]]
-  (when (seq vs)
-    [ (map second vs) (k (first (last nvs)))]))
+  (if (seq vs)
+    (let [vars (map second vs)]
+      (cl/all
+        (fd/distinct vars)
+        (nary-plus (k (first (last nvs))) vars)))
+    cl/succeed))
 
 (defn logic-line [line pair-solver]
   (let [pairs (partition-all 2 (partition-by #(core/= (type (first %)) (type kakuro.core/v)) line))]
-    (map pair-solver pairs)))
+    (cl/everyg pair-solver pairs)))
 
 (defn logic-row [row]
   (logic-line row #(logic-pair :across %)))
@@ -65,7 +69,12 @@
   (logic-line column #(logic-pair :down %)))
 
 (defn logic-grid [grid]
-  (->> grid
-       lvar-grid
-       (map logic-row)))
+  (let [lgrid (lvar-grid grid)
+        vars (->> lgrid (mapcat identity) (map second) (filter #(not (nil? %))))]
+    (if (seq vars)
+      (cl/run* [q]
+               (cl/everyg #(fd/in % (apply fd/domain (range 1 10))) vars)
+               (cl/everyg logic-row lgrid)
+               (cl/everyg logic-column (kakuro.core/transpose lgrid))
+               (cl/== q vars)))))
 
