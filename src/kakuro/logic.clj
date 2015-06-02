6e54ec45a2ffc823dabd908817eae0355ecd77f5
James Petry <gavilancomun@gmail.com>
2015-06-02 16:49:59 +0100

    logic

--- src/kakuro/logic.clj
+++ src/kakuro/logic.clj
@@ -42,7 +42,7 @@
 (defn logic-grid [grid]
   (let [lgrid (lvar-grid grid)
         vars (->> lgrid flatten (filter cl/lvar?))
-        var-grid (mapv #(->> % (map second) (filter cl/lvar?) (into [])) lgrid)]
+        var-grid (mapv #(->> % (map second) (filter cl/lvar?) vec) lgrid)]
     (if (seq vars)
       (cl/run* [q]
                (cl/everyg #(fd/in % (apply fd/domain (range 1 10))) vars)
