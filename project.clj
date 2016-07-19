440637b15c2fb6a29e7b41ceddcd5b68c58a744a
James Petry <gavilancomun@gmail.com>
2016-07-19 14:27:37 +0100

    1..9

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject kakuro-clj "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.9.0-alpha7"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha8"]
                  [org.clojure/core.logic "0.8.10"]
                  [org.clojure/test.check "0.9.0"]
                  [pjstadig/humane-test-output "0.8.0"]]
---
+++ ws/clojure.clj
@@ -0,0 +1,87 @@
+;; gorilla-repl.fileformat = 1
+
+;; **
+;;; # Gorilla REPL
+;;; 
+;;; Welcome to gorilla :-)
+;;; 
+;;; Shift + enter evaluates code. Hit alt+g twice in quick succession or click the menu icon (upper-right corner) for more commands ...
+;;; 
+;;; It's a good habit to run each worksheet in its own namespace: feel free to use the declaration we've provided below if you'd like.
+;; **
+
+;; @@
+(ns gavilan.clojure
+  (:require [clojure.pprint :as pp])
+  (:require [gorilla-plot.core :as plot]))
+;; @@
+
+;; @@
+(count {:a 1 :b 2})
+;; @@
+
+;; @@
+(count #{:a :b})
+;; @@
+
+;; @@
+(first [])
+;; @@
+
+;; @@
+(first nil)
+;; @@
+
+;; @@
+(def next-states {:satisfied #{}
+                  :cancelled #{}
+                  :booked #{:cancelled :satisfied :required}
+                  :required #{:booked :cancelled}})
+
+
+(defn valid-next? [item next-state]
+  (println next-state (next-states next-state))
+  (and item
+       (-> next-states
+           ((:state item))
+           next-state)))
+
+(def item {:state :required})
+
+(valid-next? item :booked)
+
+
+;; @@
+;; ->
+;;; :booked #{:satisfied :cancelled :required}
+;;; 
+;; <-
+;; =>
+;;; {"type":"html","content":"<span class='clj-keyword'>:booked</span>","value":":booked"}
+;; <=
+
+;; @@
+(-> next-states :required)
+;; @@
+;; =>
+;;; {"type":"list-like","open":"<span class='clj-set'>#{</span>","close":"<span class='clj-set'>}</span>","separator":" ","items":[{"type":"html","content":"<span class='clj-keyword'>:cancelled</span>","value":":cancelled"},{"type":"html","content":"<span class='clj-keyword'>:booked</span>","value":":booked"}],"value":"#{:cancelled :booked}"}
+;; <=
+
+;; @@
+(-> (next-states :required) :booked)
+;; @@
+;; =>
+;;; {"type":"html","content":"<span class='clj-keyword'>:booked</span>","value":":booked"}
+;; <=
+
+;; @@
+(-> #{:a :b} :a)
+
+;; @@
+;; =>
+;;; {"type":"html","content":"<span class='clj-keyword'>:a</span>","value":":a"}
+;; <=
+
+;; @@
+
+;; @@
